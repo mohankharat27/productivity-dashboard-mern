c4fbcb8251f009a084c53de0c3d9f2c569b74b25
mohankharat27 <kharatmohan2244@gmail.com>
2025-12-26 14:17:12 +0530

    feat: add Sidebar component to Dashboard page

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+function Sidebar() {
+  return (
+    <nav>
+      <div className="nav-in">
+        <h2>Productivity DashBoard</h2>
+        <div className="theme">
+          <h4>Change theme</h4>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Sidebar;
